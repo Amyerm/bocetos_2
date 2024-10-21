d95b26b413df751eb0255fdbcf33595e1fd2d48c
alumno <alumno@pc-candelap.uacj.mx>
2024-10-21 10:39:31 -0600

    Avance

---
+++ Bocetos_2/Modelos/Usuarios.swift
@@ -0,0 +1,13 @@
+//
+//  Usuarios.swift
+//  Bocetos_2
+//
+//  Created by alumno on 10/21/24.
+//
+
+struct Usuario: Codable{
+    var id: Int
+    var name: String
+    var username: String
+    var email: String
+}
--- Bocetos_2/proveedor/provedor_publicaciones.swift
+++ Bocetos_2/proveedor/provedor_publicaciones.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class ProveedorDePublicaciones{
-    let url_de_publicaciones = "https://jsonplaceholder.typicode.com/posts"
+    let url_de_publicaciones = "https://jsonplaceholder.typicode.com/"
     var  lista_de_publicaciones: [Publicacion] = []
     
     /*
@@ -38,7 +38,7 @@ class ProveedorDePublicaciones{
                         print(respuesta)
                     }
                 } catch {
-                    print("Error")
+                    print("Error :(")
                 }
         }.resume()
     }
@@ -69,4 +69,44 @@ class ProveedorDePublicaciones{
                 (datos, respuesta, error) in do {}
             }.resume()
         }
+    
+    func obtener_usuario(id: Int, que_hacer_al_recibir: @escaping (Usuario) -> Void) {
+    // func obtener_publicaicones() async throws -> [Publicacion] {
+        let ubicacion = URL(string: "\(url_de_publicaciones)users/\(id)")!
+        URLSession.shared.dataTask(with: ubicacion) {
+                (datos, respuesta, error) in do {
+                    if let publicaciones_recibidas = datos{
+                        let prueba_de_interpretacion_de_datos = try JSONDecoder().decode(Usuario.self, from: publicaciones_recibidas)
+                        
+                        que_hacer_al_recibir(prueba_de_interpretacion_de_datos)
+                    }
+                    else {
+                        print(respuesta)
+                    }
+                } catch {
+                    print("Error :)")
+                }
+        }.resume()
+    }
+    
+    
+    func obtener_comentarios_en_publicacion(id: Int, que_hacer_al_recibir: @escaping ([Comentario]) -> Void) {
+    // func obtener_publicaicones() async throws -> [Publicacion] {
+        let ubicacion = URL(string: "\(url_de_publicaciones)posts/\(id)/comments")!
+        URLSession.shared.dataTask(with: ubicacion) {
+                (datos, respuesta, error) in do {
+                    if let publicaciones_recibidas = datos{
+                        let prueba_de_interpretacion_de_datos = try JSONDecoder().decode([Comentario].self, from: publicaciones_recibidas)
+                        
+                        que_hacer_al_recibir(prueba_de_interpretacion_de_datos)
+                    }
+                    else {
+                        print(respuesta)
+                    }
+                } catch {
+                    print("Error :(")
+                }
+        }.resume()
+    }
+    
 }
