1318169b6d4fc9506a3612dbee3709ba61daae3b
alumno <alumno@pc-candelap.uacj.mx>
2024-10-30 10:43:08 -0600

    Se ven los comentarios

--- Bocetos_2/controladores_de_pantalla/ControladorPantallaDelPost.swift
+++ Bocetos_2/controladores_de_pantalla/ControladorPantallaDelPost.swift
@@ -17,108 +17,107 @@ class ControladorPantallaDelPost: UIViewController, UICollectionViewDataSource {
     @IBOutlet weak var titulo_de_publicaciones: UILabel!
     @IBOutlet weak var nombre_de_usuario: UILabel!
     @IBOutlet weak var cuerpo_de_publicacion: UILabel!
-    
     @IBOutlet weak var seccion_comentarios: UICollectionView!
     
-    
     public var id_publicacion: Int?
-    
-    private var publicacion: Publicacion?
+        
+        private var publicacion:Publicacion?
         private var usuario: Usuario?
         private var lista_comentarios: [Comentario] = []
+        
 
         override func viewDidLoad() {
-            super.viewDidLoad()
-            
-
-            // Do any additional setup after loading the view.
-            
-            
-            let controlador_de_navegacion = self.navigationController as? mod_navegador_principal
-            controlador_de_navegacion?.activar_navigation_bar(actviar: true)
-            
-            seccion_comentarios.dataSource = self
-            
-            realizar_descarga_de_informacion()
-        }
-        
-        func realizar_descarga_de_informacion(){
-            if self.publicacion == nil {
-                proveedor_publicaciones.obtener_publicacion(id: self.id_publicacion ?? -1, que_hacer_al_recibir: {
-                    [weak self] (publicacion) in self?.publicacion = publicacion
-                    DispatchQueue.main.async {
-                        self?.dibujar_publicacion()
-                        self?.realizar_descarga_de_informacion()
-                    }
-                })
-            }
-            
-            else if self.publicacion != nil {
-                proveedor_publicaciones.obtener_usuario(id: publicacion!.userId, que_hacer_al_recibir: {
-                    [weak self] (usuario) in self?.usuario = usuario
-                    DispatchQueue.main.async {
-                        self?.dibujar_usuario()
-                    }
-                })
+                super.viewDidLoad()
                 
-                proveedor_publicaciones.obtener_comentarios_en_publicacion(id: publicacion!.id, que_hacer_al_recibir: {
-                    [weak self] (comentarios_descargados) in self?.lista_comentarios = comentarios_descargados
-                    DispatchQueue.main.async {
-                        self?.seccion_comentarios.reloadData()
-                    }
-                })
+
+                // Do any additional setup after loading the view.
+                
+                
+                let controlador_de_navegacion = self.navigationController as? mod_navegador_principal
+                controlador_de_navegacion?.activar_navigation_bar(actviar: true)
+                
+                seccion_comentarios.dataSource = self
+                
+                realizar_descarga_de_informacion()
             }
             
-            
-            
-        }
-        
-        func dibujar_publicacion(){
-            guard let publicacion_actual = self.publicacion else {
-                return
+            func realizar_descarga_de_informacion(){
+                if self.publicacion == nil {
+                    proveedor_publicaciones.obtener_publicacion(id: self.id_publicacion ?? -1, que_hacer_al_recibir: {
+                        [weak self] (publicacion) in self?.publicacion = publicacion
+                        DispatchQueue.main.async {
+                            self?.dibujar_publicacion()
+                            self?.realizar_descarga_de_informacion()
+                        }
+                    })
+                }
+                
+                else if self.publicacion != nil {
+                    proveedor_publicaciones.obtener_usuario(id: publicacion!.userId, que_hacer_al_recibir: {
+                        [weak self] (usuario) in self?.usuario = usuario
+                        DispatchQueue.main.async {
+                            self?.dibujar_usuario()
+                        }
+                    })
+                    
+                    proveedor_publicaciones.obtener_comentarios_en_publicacion(id: publicacion!.id, que_hacer_al_recibir: {
+                        [weak self] (comentarios_descargados) in self?.lista_comentarios = comentarios_descargados
+                        DispatchQueue.main.async {
+                            self?.seccion_comentarios.reloadData()
+                        }
+                    })
+                }
+                
+                
+                
             }
             
-            titulo_de_publicaciones.text = publicacion_actual.title
-            cuerpo_de_publicacion.text = publicacion_actual.body
-            
-        }
-        
-        func dibujar_usuario(){
-            guard let usuario_actual = self.usuario else {
-                return
+            func dibujar_publicacion(){
+                guard let publicacion_actual = self.publicacion else {
+                    return
+                }
+                
+                titulo_de_publicaciones.text = publicacion_actual.title
+                cuerpo_de_publicacion.text = publicacion_actual.body
+                
             }
             
-            nombre_de_usuario.text = usuario_actual.username
+            func dibujar_usuario(){
+                guard let usuario_actual = self.usuario else {
+                    return
+                }
+                
+                nombre_de_usuario.text = usuario_actual.username
+                
+            }
             
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return lista_comentarios.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            print("Aqui deberia hacer algo")
-            let celda_comentario: VistaDeComentario = collectionView.dequeueReusableCell(withReuseIdentifier: identificador_de_celda, for: indexPath) as! VistaDeComentario
-        
-            celda_comentario.comentario.text = self.lista_comentarios[indexPath.item].body
+            func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                print("\(lista_comentarios.count)")
+                return lista_comentarios.count
+            }
             
-            // Configure the cell
-            //celda.tintColor = UIColor.green
-
-            // print(self.lista_de_publicaciones)
+            func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                print("Aqui denberia hacer algo")
+                let celda_comentario = collectionView.dequeueReusableCell(withReuseIdentifier: identificador_de_celda, for: indexPath) as! VistaDeComentario
+                
+                celda_comentario.comentario.text = self.lista_comentarios[indexPath.item].body
             
-            return celda_comentario
+                // Configure the cell
+                //celda.tintColor = UIColor.green
+          
+                // print(self.lista_de_publicaciones)
+                
+                return celda_comentario
+            }
+            
+            /*
+            // MARK: - Navigation
+
+            // In a storyboard-based application, you will often want to do a little preparation before navigation
+            override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                // Get the new view controller using segue.destination.
+                // Pass the selected object to the new view controller.
+            }
+            */
+
         }
-        
-
-        /*
-        // MARK: - Navigation
-
-        // In a storyboard-based application, you will often want to do a little preparation before navigation
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            // Get the new view controller using segue.destination.
-            // Pass the selected object to the new view controller.
-        }
-        */
-
-    }
